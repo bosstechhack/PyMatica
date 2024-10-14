b7995b593e4fc37423a3b2f66d518aa920581d8d
Ashish Ranjan <funboss043@gmail.com>
2024-10-14 17:08:48 +0530

    removed imports

--- pymatica/__init__.py
+++ pymatica/__init__.py
@@ -5,12 +5,3 @@ __email__ = "admin@example.com"
 __version__ = "2.1.2"
 
 
-from .basic_statistics import BasicStatistics
-from .matrix_operations import MatrixOperations
-from .polynomial import Polynomial
-from .graph_plotter import SimpleGraphPlotter
-from .math_constants import MathConstants
-from .combinatorial import CombinatorialFunctions
-from .geometry_utils import GeometryUtilities
-from .number_theory import NumberTheoryTools
-
